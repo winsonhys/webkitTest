55446fc2e84676e21382f0cd4784d000fd0935be
WInson <winsonheng@hotmail.com>
2019-07-30 10:15:32 -0700

    componentdidmount

--- src/App.js
+++ src/App.js
@@ -1,50 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Button } from 'semantic-ui-react'
-window.androidObj = function AndroidClass(){};
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "semantic-ui-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button
-          onClick={() => {
-            let message = {
-              test: "test"
-            };
-            
-            try {
-              window.androidObj.textToAndroid("anant");
-              console.log("lawl");
-            } catch (e) {
-              console.log(e)
-            }
-            try {
-              window.webkit.messageHandlers.test.postMessage(
-                message
-              );
-              console.log("lawl");
-            } catch (e) {
-              console.log(e)
-            }
-          }}
-        >Click this to trigger event</Button>
-      </header>
-    </div>
-  );
+class App extends React {
+  
+  componentDidMount() {
+    window.androidObj = function AndroidClass() {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <Button
+            onClick={() => {
+              let message = {
+                test: "test"
+              };
+
+              try {
+                window.androidObj.textToAndroid("anant");
+                console.log("lawl");
+              } catch (e) {
+                console.log(e);
+              }
+              try {
+                window.webkit.messageHandlers.test.postMessage(message);
+                console.log("lawl");
+              } catch (e) {
+                console.log(e);
+              }
+            }}
+          >
+            Click this to trigger event
+          </Button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
