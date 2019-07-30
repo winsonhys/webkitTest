b14658189646c317f4d49525485fbcd4e3f37d29
Anant <anant.kandoi@vida.com>
2019-07-30 13:44:35 -0700

    sucks

--- src/App.js
+++ src/App.js
@@ -47,19 +47,6 @@ class App extends React.Component {
           >
             Click this to trigger event
           </Button>
-
-          <Button
-            onClick={() => {
-              try {
-                window.androidObj.navToAndroid("YEET");
-                console.log("lawl");
-              } catch (e) {
-                console.log(e);
-              }
-            }}
-          >
-            Click this to nav back
-          </Button>
         </header>
       </div>
     );
